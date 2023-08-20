4ef23abcaaa34cd8e1b44b697c5fda12855807fc
laxmi.chittidi <laxmi.chittidi@gmail.com>
2023-08-20 09:32:03 -0700

    Initial commit

---
+++ spring5-receipe-app/src/main/java/com/springmvc/receipeapp/Spring5ReceipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.receipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReceipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5ReceipeAppApplication.class, args);
+	}
+
+}
---
+++ spring5-receipe-app/src/test/java/com/springmvc/receipeapp/Spring5ReceipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.receipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ReceipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
