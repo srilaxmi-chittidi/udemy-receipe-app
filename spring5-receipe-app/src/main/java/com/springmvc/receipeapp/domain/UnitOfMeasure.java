41f17782c109016fce409fafbd5e0be987008b95
laxmi.chittidi <laxmi.chittidi@gmail.com>
2023-08-25 17:08:58 -0700

    Added assignment

--- spring5-receipe-app/src/main/java/com/springmvc/receipeapp/domain/UnitOfMeasure.java
+++ spring5-receipe-app/src/main/java/com/springmvc/receipeapp/domain/UnitOfMeasure.java
@@ -1,56 +1,43 @@
 package com.springmvc.receipeapp.domain;
 
-import java.math.BigDecimal;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String description;
-	private BigDecimal amount;
-	@ManyToOne
-	private Recipe recipe;
+	@OneToOne(fetch = FetchType.EAGER)
+	private Ingredient ingredient;
 	
-	@OneToOne
-	private UnitOfMeasure uom;
-	
-	public UnitOfMeasure getUom() {
-		return uom;
-	}
-	public void setUom(UnitOfMeasure uom) {
-		this.uom = uom;
-	}
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
-	}
-	public BigDecimal getAmount() {
-		return amount;
-	}
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
-	public Recipe getRecipe() {
-		return recipe;
-	}
-	public void setRecipe(Recipe recipe) {
-		this.recipe = recipe;
 	}
 }
